6dbfbd13ca698af0da0a9eff1f00c0f00e5ce2a4
Pablo Palomo III <pablopalomo3@gmail.com>
2017-12-13 19:51:12 -0600

    assignment 9 directives pt1: completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -33,7 +33,7 @@
                 };
 
                 scope.thumbStyle = function() {
-                    return {width: percentString()};
+                    return {left: percentString()};
                 };
 
                 scope.onClickSeekBar = function(event) {
